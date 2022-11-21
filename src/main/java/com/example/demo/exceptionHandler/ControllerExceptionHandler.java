1327e908444526a2d6262fca56f3c054f5265db4
lucaspiritogit <lucasignaciopirito@gmail.com>
2022-11-20 22:12:50 -0300

    improved exception handler

--- src/main/java/com/example/demo/exceptionHandler/ControllerExceptionHandler.java
+++ src/main/java/com/example/demo/exceptionHandler/ControllerExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.example.demo.exceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,7 +15,9 @@ public class ControllerExceptionHandler {
 	@ExceptionHandler(CustomException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ResponseBody
-	public Object handleException(Exception ex) {
-		return ex.getMessage();
+	public Map<String, String> handleException(Exception ex) {
+		Map<String, String> errorMessage = new HashMap<>();
+		errorMessage.put("error", ex.getMessage());
+		return errorMessage;
 	}
 }
